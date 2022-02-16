3fc4c66bc7e2ae0ae11d7dfbe75a1b73a18a9aa6
Antharia <artenischloria@gmail.com>
2022-02-16 10:49:52 +0100

    FIX Articles

--- articles/models.py
+++ articles/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.urls import reverse
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
 
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50)
-    content = models.TextField(max_length=1000)
+    content = MarkdownxField()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("article_detail", args=[str(self.slug)])
+
+    def formatted_markdown(self):
+        return markdownify(self.content)
 

9c3a8164dec702b1e105d21318d093dd51ce7c39
Antharia <artenischloria@gmail.com>
2022-02-20 22:27:13 +0100

    FIX articles by tag

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.article_list, name="article_list"),
     path("<slug:slug>", views.article_detail, name="article_detail"),
+    path("tags/<slug:slug>", views.articles_by_tag, name="articles_by_tag"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -7,7 +7,23 @@ def article_list(request):
     context = {"article_list": article_list}
     return render(request, "articles/article_list.html", context)
 
+
 def article_detail(request, slug):
     article = get_object_or_404(Article.objects.all(), slug=slug)
     context = {"article": article}
     return render(request, "articles/article_detail.html", context)
+
+
+def articles_by_tag(request, slug):
+    article_list = Article.objects.filter(tags__slug=slug)
+    context = {"article_list": article_list, "slug": slug}
+    return render(request, "articles/articles_by_tag.html", context)
+
+
+#  class ArticleTagListView(ListView):
+#      model = Article
+#      context_object_name = "article_list"
+#      template_name = "articles/articles_by_tag.html"
+#
+#      def get_queryset(self):
+#          return Article.objects.filter(tags__slug=self.kwargs.get("tag_slug"))
